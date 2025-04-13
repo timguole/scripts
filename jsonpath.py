d21d68fad345561ee5668ddbed24ca95dc201021
Guo Le <foobarnull@163.com>
2025-04-13 12:45:36 +0800

    Refactor jsonpath

--- jsonpath.py
+++ jsonpath.py
@@ -2,17 +2,28 @@
 
 from shlex import shlex
 import sys
-import re
 import io
 
 
 class JSONPath:
-	'''
-	Split a path into path elements. The default elements seperator is
-	slash (/)
+	'''Find data in a json object
+	Path starts with a `/` and each component in a path is either
+	index of a list or key in a dict. `*` means all element in a list
+	or all values in a dict. `?` means all keys in a dict or the
+	length of a list. And `?` is only valid as the last component of
+	a path.
+
+	Path examples:
+		"/a/b/c"
+		"/a/*/c"
+		"/a/21/c"
+		"/a/?"
 	e.g.
+		import io
+		import json
 		jp = JSONPath("/a/b/c")
-		pl = list(jp.items()) # pl is ["a", "b", "c"]
+		sio = io.StringIO('{"{'foo': ['bar', 'zoo']}")
+		data = jp.find(json.load(sio))
 	'''
 	def __init__(self, path, sep='/'):
 		'''
@@ -50,72 +61,55 @@ class JSONPath:
 		self.ready = True
 
 
-	def items(self):
+	def __parse(self):
 		'''
 		This method returns a generator
 		'''
 		if not self.ready:
 			self.__split()
-		for e in self.elements:
-			yield e
 
 
-def get_object(obj, path):
-	''' Get object from a loaded json document.
-
-		obj: the loaded json document
-		path: filesystem-path-like string to the sub-object.
-
-		Path starts with a `/` and each component in a path is either
-		index of a list or key in a dict. `*` means all element in a list
-		or all values in a dict. `?` means all keys in a dict or the
-		length of a list. And `?` is only valid as the last component of
-		a path.
-
-		Path examples:
-			"/a/b/c"
-			"/a/*/c"
-			"/a/21/c"
-			"/a/?"
-		'''
-	def _find(o, k, v):
-		if isinstance(o, list):
-			if len(k) == 1:
-				if k[0] == '*':
-					v.extend(o)
-				elif k[0] == '?':
-					v.append(len(o))
+	def find(self, json_object):
+		''' json_object: the loaded json document'''
+		def _find(o, k, v):
+			if isinstance(o, list):
+				if len(k) == 1:
+					if k[0] == '*':
+						v.extend(o)
+					elif k[0] == '?':
+						v.append(len(o))
+					else:
+						v.append(o[int(k[0])])
+				else: # there are still more than one path elements
+					if k[0] == '*':
+						for _o in o:
+							_find(_o, k[1:], v)
+					elif k[0] == '?':
+						raise SyntaxError("'?' can only appear at the last")
+					else:
+						_find(o[int(k[0])], k[1:], v)
+			elif isinstance(o, dict):
+				if len(k) == 1:
+					if k[0] == '*':
+						for key, val in o.items():
+							v.append(val)
+					elif k[0] == '?':
+						v.extend(o.keys())
+					else:
+						v.append(o[k[0]])
 				else:
-					v.append(o[int(k[0])])
-			else: # there are still more than one path elements
-				if k[0] == '*':
-					for _o in o:
-						_find(_o, k[1:], v)
-				elif k[0] == '?':
-					raise SyntaxError("'?' can only appear at the last")
-				else:
-					_find(o[int(k[0])], k[1:], v)
-		elif isinstance(o, dict):
-			if len(k) == 1:
-				if k[0] == '*':
-					for key, val in o.items():
-						v.append(val)
-				elif k[0] == '?':
-					v.extend(o.keys())
-				else:
-					v.append(o[k[0]])
-			else:
-				if k[0] == '*':
-					for key, val in o.items():
-						_find(val, k[1:], v)
-				elif k[0] == '?':
-					raise SyntaxError("'?' can only appear at the last")
-				else:
-					_find(o[k[0]], k[1:], v)
+					if k[0] == '*':
+						for key, val in o.items():
+							_find(val, k[1:], v)
+					elif k[0] == '?':
+						raise SyntaxError("'?' can only appear at the last")
+					else:
+						_find(o[k[0]], k[1:], v)
 
-	values = []
-	_find(obj, path, values)
-	return values
+		values = []
+		self.__parse()
+		_find(json_object, self.elements, values)
+		return values
 
 
 def printerr(msg):
@@ -143,12 +137,14 @@ if __name__ == '__main__':
 
 	try:
 		jobject = json.load(json_file)
+		jp = JSONPath(path_str, seperator)
+		v = jp.find(jobject)
+		print(v)
 	except json.JSONDecodeError as e:
 		printerr('Failed to decode the input')
 		printerr(e)
 		exit(1)
-	jp = JSONPath(path_str, seperator)
-	path_list = list(jp.items())
-	v = get_object(jobject, path_list)
-	print(v)
+	except Exception as e:
+		printerr(e)
+		exit(1)
 
